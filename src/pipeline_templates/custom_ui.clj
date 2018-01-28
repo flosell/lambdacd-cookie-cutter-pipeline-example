ee42e4b4ed836f8758ad3efb531248811c8a46e0
Florian Sellmayr <florian.sellmayr@gmail.com>
2018-01-28 13:58:28 +0100

    Change UI page instead of clonging it using deprecated functions

--- src/pipeline_templates/custom_ui.clj
+++ src/pipeline_templates/custom_ui.clj
@@ -3,7 +3,11 @@
             [lambdacd.ui.api :as api]
             [lambdacd.ui.ui-page :as ui-page]
             [compojure.core :refer [routes GET context]]
-            [hiccup.core :as h]))
+            [hickory.core :as hickory]
+            [hickory.render :as hickory-render]
+            [hickory.convert :as hickory-convert]
+            [hickory.select :as hickory-select]
+            [clojure.zip :as zip]))
 
 (defn- navbar [projects]
   [:ul {:class "navbar"}
@@ -19,27 +23,41 @@
      [:span {:class "app__header__pipeline-name"} pipeline-name])
    (navbar projects)])
 
-(defn- ui-page [projects pipeline]
-  (let [pipeline-name (get-in pipeline [:context :config :name])
-        ui-config-data (get-in pipeline [:context :config :ui-config])]
-    (h/html
-      [:html
-       [:head
-        [:title (str pipeline-name " - LambdaCD")]
-        (ui-page/favicon)
-        (ui-page/css-includes)
-        (ui-page/ui-config ui-config-data)
-        [:style ".navbar li { display: inline; padding-right: 20px; }"]]
-       [:body
-        [:div {:class "app l-horizontal"}
-         (header projects pipeline-name)
-         (ui-page/app-placeholder)]
-        (ui-page/js-includes)]])))
+(defn- replace-title [hic pipeline-name]
+  (let [locs (hickory-select/select-locs (hickory-select/tag :title) hic)]
+    (if (empty? locs)
+      (throw (Exception. "no locs found")))
+    (zip/replace (first locs) {:type :element :tag :title :attrs nil :content [(str pipeline-name " - LambdaCD")]})))
+
+(defn- append-style [hic style]
+  (let [locs (hickory-select/select-locs (hickory-select/tag :head) hic)]
+    (if (empty? locs)
+      (throw (Exception. "no locs found")))
+    (zip/append-child (first locs) (first (hickory-convert/hiccup-fragment-to-hickory [[:style style]])))))
+
+(defn- replace-header [hic header]
+  (let [locs (hickory-select/select-locs (hickory-select/class "app__header") hic)]
+    (if (empty? locs)
+      (throw (Exception. "no locs found")))
+    (zip/replace (first locs) (first (hickory-convert/hiccup-fragment-to-hickory [header])))))
+
+(defn new-ui-page [projects pipeline]
+  (let [pipeline-name  (get-in pipeline [:context :config :name])]
+    (-> (ui-page/ui-page pipeline)
+        (hickory/parse)
+        (hickory/as-hickory)
+        (replace-title pipeline-name)
+        (zip/root)
+        (append-style ".navbar li { display: inline; padding-right: 20px; }")
+        (zip/root)
+        (replace-header (header projects pipeline-name))
+        (zip/root)
+        (hickory-render/hickory-to-html))))
 
 (defn ui-for
   ([pipeline projects]
    (routes
      (context "/api" [] (api/rest-api pipeline))
-     (GET "/" [] (ui-page projects pipeline))
+     (GET "/" [] (new-ui-page projects pipeline))
      (route/resources "/" {:root "public"}))))
 
